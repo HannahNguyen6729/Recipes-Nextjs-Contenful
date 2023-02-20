ba7a8d0bc6a51f85006bddb43d1b7acc1825e97b
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-02-20 20:56:03 +0200

    render [slug] page, Rich Text Content with rich-text-react-renderer

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -40,6 +42,56 @@ export async function getStaticProps(context) {
 }
 
 export default function RecipeDetails({ recipe }) {
-  // console.log("recipe prop", recipe);
-  return <div>Recipe Details</div>;
+  console.log("recipe prop", recipe);
+  const { title, ingredients, method, featured, cookingTime } = recipe.fields;
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={`https:${featured.fields.file.url}`}
+          width={featured.fields.file.details.image.width}
+          height={featured.fields.file.details.image.height}
+          alt={title}
+        />
+        <h2>{title}</h2>
+      </div>
+
+      <div className="info">
+        <p>Take about {cookingTime} mins to cook</p>
+        <h3>Ingredients:</h3>
+        {ingredients.map((ingredient) => (
+          <span key={ingredient}> {ingredient} </span>
+        ))}
+      </div>
+
+      <div className="method">{documentToReactComponents(method)}</div>
+
+      <style jsx>{`
+        h2,
+        h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
+    </div>
+  );
 }
