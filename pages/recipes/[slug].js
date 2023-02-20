2c2d1daaf97fa7b7852a2f153c3e23905d766899
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-02-20 20:16:47 +0200

    generating paths

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,45 @@
-export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticPaths() {
+  const response = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const recipes = response.items;
+  const paths = recipes.map((recipe) => {
+    return {
+      params: { slug: recipe.fields.slug },
+    };
+  });
+  //console.log("paths", paths);
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  const recipe = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": slug,
+  });
+
+  console.log("recipe", recipe.items[0]);
+
+  return {
+    // Passed to the page component as props
+    props: { recipe: recipe.items[0] },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  // console.log("recipe prop", recipe);
+  return <div>Recipe Details</div>;
 }
